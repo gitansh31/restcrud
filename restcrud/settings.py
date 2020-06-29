2201a7e8c3c05148334997308623d7f6a9197d00
gitansh31 <54281336+gitansh31@users.noreply.github.com>
2020-06-29 11:24:18 +0530

    removed secret key

--- restcrud/settings.py
+++ restcrud/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = '4+55vck0nyvnhm*no_!6(t9s@xb@9u*k&b7z3wnqu-hgzs_1$w'
+
 import os
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','4+55vck0nyvnhm*no_!6(t9s@xb@9u*k&b7z3wnqu-hgzs_1$w')
 # SECURITY WARNING: don't run with debug turned on in production!
